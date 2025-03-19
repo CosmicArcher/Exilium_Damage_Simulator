a0ec304b3c862fff152f92cce5a42358fdeb6513
CosmicArcher <walkerumbra@gmail.com>
2025-03-19 15:37:38 +0000

    Modified game state manager in preparation for rewind

--- GameStateManager.js
+++ GameStateManager.js
@@ -1,5 +1,3 @@
-import Target from "./Target.js";
-
 let GameStateManagerSingleton;
 
 class GameStateManager {
@@ -23,22 +21,26 @@ class GameStateManager {
     // track total damage for ease of strat comparison
     addTotalDamage(x) {
         if (GameStateManagerSingleton)
-            GameStateManagerSingleton.totalDamage += x;
+            GameStateManagerSingleton.totalDamage[actionCount+1] += x;
         else
             console.error("Singleton not yet initialized");
     }
     getTotalDamage() {
         if (GameStateManagerSingleton)
-            return GameStateManagerSingleton.totalDamage;
+            return GameStateManagerSingleton.totalDamage[actionCount+1];
         else
             console.error("Singleton not yet initialized");
     }
     // return to a clean slate to test a completely different setup
     resetSimulation() {
         if (GameStateManagerSingleton) {
-            GameStateManagerSingleton.totalDamage = 0;
-            GameStateManagerSingleton.target = null;
+            GameStateManagerSingleton.totalDamage = [0];
+            GameStateManagerSingleton.target = [null];
             GameStateManagerSingleton.coverValue = 0;
+            GameStateManagerSingleton.dolls = [[]];
+            // rewinding will be implemented as each action creating a clone of all units and modifying them when using skills and applying buffs
+            // rewinding to a specific action will delete all indices after that action
+            GameStateManagerSingleton.actionCount = 0;
         }
         else
             console.error("Singleton not yet initialized");
@@ -46,15 +48,42 @@ class GameStateManager {
     // game state manager holds reference to the target
     registerTarget(target) {
         if (GameStateManagerSingleton) {
-            GameStateManagerSingleton.target = target;
+            GameStateManagerSingleton.target[0] = target;
         }
         else
             console.error("Singleton not yet initialized");
     }
     getTarget() {
         if (GameStateManagerSingleton) {
-            return GameStateManagerSingleton.target;
+            return GameStateManagerSingleton.target[GameStateManagerSingleton.actionCount+1];
         }
+        else
+            console.error("Singleton not yet initialized");
+    }
+    // game state manager holds references to the dolls for searching by name and cloning as rewind will require multiple copies of the same doll
+    registerDoll(doll) {
+        if (GameStateManagerSingleton) {
+            GameStateManagerSingleton.dolls[0].push(doll);
+        }
+        else
+            console.error("Singleton not yet initialized");
+    }
+    // return the latest copy of the doll
+    getDoll(dollName) {
+        if (GameStateManagerSingleton) {
+            for (let i = 0; i < GameStateManagerSingleton.dolls[GameStateManagerSingleton.actionCount+1].length; i++) {
+                if (GameStateManagerSingleton.dolls[GameStateManagerSingleton.actionCount+1][i].getName() == dollName)
+                    return GameStateManagerSingleton.dolls[GameStateManagerSingleton.actionCount+1][i];
+            }
+            console.error(`${dollName} does not exist in Game State Manager singleton`);
+        }
+        else
+            console.error("Singleton not yet initialized");
+    }
+    // return the latest copies of all the dolls
+    getAllDolls() {
+        if (GameStateManagerSingleton) 
+            return GameStateManagerSingleton.dolls[GameStateManagerSingleton.actionCount+1];
         else
             console.error("Singleton not yet initialized");
     }
@@ -73,9 +102,54 @@ class GameStateManager {
             console.error("Singleton not yet initialized");
     }
     // let the target perform their turn, ticking down any buffs/debuffs
-    endTurn() {
+    endRound() {
         if (GameStateManagerSingleton) {
-            GameStateManagerSingleton.target.endTurn();
+            GameStateManagerSingleton.target[GameStateManagerSingleton.actionCount+1].endTurn();
+        }
+        else
+            console.error("Singleton not yet initialized");
+    }
+    // "lock in" the effects of the last action and clone all units in preparation for the next action
+    lockAction() {
+        if (GameStateManagerSingleton) {
+            // clone the latest version of the units
+            let target = GameStateManagerSingleton.target[GameStateManagerSingleton.actionCount+1];
+            let dolls = GameStateManagerSingleton.dolls[GameStateManagerSingleton.actionCount+1];
+            let newTarget = target.cloneUnit();
+            let newDolls = [];
+            dolls.forEach(doll => {
+                newDolls.push(doll.cloneUnit());
+            });
+            // put the latest units as the ones in the get functions
+            GameStateManagerSingleton.target.push(newTarget);
+            GameStateManagerSingleton.dolls.push(newDolls);
+            GameStateManagerSingleton.totalDamage.push(GameStateManagerSingleton.totalDamage[GameStateManagerSingleton.actionCount+1]);
+            GameStateManagerSingleton.actionCount++;
+        }
+        else
+            console.error("Singleton not yet initialized");
+    }
+    
+    rewindToAction(actionNumber) {
+        if (GameStateManagerSingleton) {
+            GameStateManagerSingleton.actionCount = actionNumber;
+            // clone the version of the units from actionNumber and 
+            let target = GameStateManagerSingleton.target[actionNumber];
+            let dolls = GameStateManagerSingleton.dolls[actionNumber];
+            let newTarget = target.cloneUnit();
+            let newDolls = [];
+            dolls.forEach(doll => {
+                newDolls.push(doll.cloneUnit());
+            });
+            // all arrays should be the same size so just calculate the length to cut once
+            let lengthToCut = GameStateManagerSingleton.target.length - actionNumber - 1;
+            // put these new clones as the ones in the get functions by removing all entries after actionNumber then appending the clones
+            GameStateManagerSingleton.target.splice(actionNumber+1, lengthToCut);
+            GameStateManagerSingleton.dolls.splice(actionNumber+1, lengthToCut);
+            GameStateManagerSingleton.totalDamage.splice(actionNumber+1, lengthToCut);
+            GameStateManagerSingleton.target.push(newTarget);
+            GameStateManagerSingleton.dolls.push(newDolls);
+            GameStateManagerSingleton.totalDamage.push(GameStateManagerSingleton.totalDamage[actionNumber]);
         }
         else
             console.error("Singleton not yet initialized");
